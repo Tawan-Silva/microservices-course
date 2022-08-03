cd5cf717b2f9f5613a2266435b58e737de9f93fd
Tawan Silva <tawan.tls43@gmail.com>
2022-08-03 18:56:00 -0300

    Project hr-worker created

---
+++ hr-worker/src/test/java/com/tawanweb/hrwoker/HrWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tawanweb.hrwoker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
